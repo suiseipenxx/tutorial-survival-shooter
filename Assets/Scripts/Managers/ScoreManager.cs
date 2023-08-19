d9153c0889099b9856d6adaa1dbededae0184fb3
suiseipenxx <pandorabox57042@gmail.com>
2023-08-19 14:03:07 +0800

    上傳檔案

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+
+    TextMeshProUGUI text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <TextMeshProUGUI> ();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
